d81a86cc481178190a926019d4a2b30fd07c8122
tomoyat <1283480+tomoyat@users.noreply.github.com>
2021-03-29 02:23:33 +0900

    Create Layout component

---
+++ src/components/layout.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { useStaticQuery, Link, graphql } from "gatsby"
+
+export default function Layout({ children }) {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+  return (
+    <div>
+      <Link to={`/`}>
+        <h3>
+          {data.site.siteMetadata.title}
+        </h3>
+      </Link>
+      {children}
+    </div>
+  )
+}
