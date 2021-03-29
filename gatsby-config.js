12fb8c62bd9ee8d18646063b9a1b06cd958ccd88
tomoyat <1283480+tomoyat@users.noreply.github.com>
2021-03-29 18:41:22 +0900

    Add typography config

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,12 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
---
+++ src/utils/typography.js
@@ -0,0 +1,41 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.7,
+  scaleRatio: 1.9,
+  googleFonts: [
+    {
+      name: 'M+PLUS+1p',
+      styles: ['400'],
+    },
+    {
+      name: 'Noto+Sans+JP',
+      styles: ['300'],
+    },
+    {
+      name: 'Lato',
+      styles: ['400'],
+    },
+    {
+      name: 'Roboto',
+      styles: ['300'],
+    },
+  ],
+  headerFontFamily: [
+    "Lato",
+    'M PLUS 1p',
+  ],
+  bodyFontFamily: [
+    "Roboto",
+    'Noto Sans JP',
+  ],
+  overrideStyles: ({ scale, rhythm }, options, styles) => ({
+    p: {
+      textAlign: 'justify',
+      wordBreak: 'break-all',
+    },
+  })
+})
+
+export default typography
