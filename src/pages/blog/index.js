a9c1c252ce081101b9594d0478d7234e5bfdf4c2
tomoyat <1283480+tomoyat@users.noreply.github.com>
2021-04-12 01:26:22 +0900

    Update blog top

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,17 +1,17 @@
 import React from "react"
 import Layout from "../../components/layout"
-import { Link, graphql } from "gatsby"
+import Card from "../../components/blog/card"
+import { graphql } from "gatsby"
 
-export default function Home({data}) {
+export default function Blog({ data }) {
   return (
     <Layout>
-      {data.allMarkdownRemark.edges.map( ({node}) => (
+      {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
-          <Link to={node.fields.slug}>
-            <h3>{node.frontmatter.title}</h3>
-            <p>{node.frontmatter.date}</p>
-            <p>{node.excerpt}</p>
-          </Link>
+          <Card slug={node.fields.slug}
+                title={node.frontmatter.title}
+                date={node.frontmatter.date}
+                excerpt={node.excerpt} />
         </div>
       ))}
 
