3338e2a62719a57a60e0c1b5465e94ca0ebf4b89
tomoyat <1283480+tomoyat@users.noreply.github.com>
2021-09-02 00:02:43 +0900

    Update top page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,41 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
+import Card from "../components/blog/card"
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <Layout>
-      <div>Hello world!</div>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <Card slug={node.fields.slug}
+                title={node.frontmatter.title}
+                date={node.frontmatter.date}
+                excerpt={node.excerpt} />
+        </div>
+      ))}
+
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "YYYY/MM/DD")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
