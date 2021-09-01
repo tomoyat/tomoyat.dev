82c1def0e2fe53226f5f9789f6554a72602fac3e
tomoyat <1283480+tomoyat@users.noreply.github.com>
2021-09-02 00:24:42 +0900

    Add title

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,25 @@
 import React from "react"
 import Header from "./header"
+import { Helmet } from "react-helmet"
+import { graphql, useStaticQuery } from "gatsby"
 export default function Layout({ children }) {
-
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{data.site.siteMetadata.title}</title>
+      </Helmet>
       <Header />
       <div style={{ margin: `2rem auto`, maxWidth: 960 }}>
       {children}
