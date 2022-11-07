71d51c4ac37fa81a3ee69d5804310257fca825c0
tomoyat <1283480+tomoyat@users.noreply.github.com>
2022-11-08 01:08:40 +0900

    impl list view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,18 @@
     posts.sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime())
 </script>
 
-{#each posts as post}
-    <a href="/posts/{post.slug}">{post.title}</a>
-{/each}
+<div class="mt-4">
+    {#each posts as post, i}
+        {#if i < 1}
+            <div class="mt-12">
+                <a href="/posts/{post.slug}"><h2 class="text-xl text-navy-dark">{post.title}</h2></a>
+                <div class="mt-2 text-sm text-navy-light">{post.publishedAtString}</div>
+            </div>
+        {:else}
+            <div class="mt-8">
+                <a href="/posts/{post.slug}"><h2 class="text-xl text-navy-dark">{post.title}</h2></a>
+                <div class="mt-2 text-sm text-navy-light">{post.publishedAtString}</div>
+            </div>
+        {/if}
+    {/each}
+</div>
