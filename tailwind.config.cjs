17f7b89714b0473419418ee89f7aba12a084bcda
tomoyat <1283480+tomoyat@users.noreply.github.com>
2022-11-07 23:31:46 +0900

    色の設定を追加

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    colors: {
+      'moon': {
+        DEFAULT: '#EAF4FC'
+      },
+      'navy': {
+        light: '#2A4073',
+        DEFAULT: '#0F2350',
+        dark: '#17184B'
+      },
+      'blue': {
+        DEFAULT: '#19448E'
+      },
+    },
     extend: {},
   },
   plugins: [
