b0bb0800a8291601f7d4a3182a52bee37c08df96
tomoyat <1283480+tomoyat@users.noreply.github.com>
2022-11-26 01:34:03 +0900

    下にマージンを追加

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="max-w-4xl w-full mx-auto mt-0">
-    <div class="mx-4">
+    <div class="mx-4 mb-10">
         <header class="h-20 mt-4">
             <a href="/" class="text-base text-navy-dark">blog.tomoyat.dev</a>
         </header>
