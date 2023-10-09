62a323686e4677a25779897b607a944795df27ee
tomoyat <1283480+tomoyat@users.noreply.github.com>
2023-10-10 01:52:16 +0900

    デザインの変更

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -19,7 +19,8 @@
 <article class="mt-12">
     <h1 class="font-medium text-2xl text-navy-dark text-center">{ post.title }</h1>
     <p class="text-sm text-navy-light mt-2 text-right">{post.publishedAtString}</p>
-    <div class="prose mt-12 tracking-wider text-justify max-w-none prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg prose-p:text-base prose-a:text-base prose-li:text-base">
+    <div class="prose mt-12 tracking-wider text-justify max-w-none prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg
+      prose-p:text-base prose-p:font-light prose-a:text-base prose-li:text-base prose-li:font-light">
         <svelte:component this={post.component}/>
     </div>
 </article>
