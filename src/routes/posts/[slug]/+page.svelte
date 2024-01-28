1ca2d0c8f906fa3e5ab438699bdb9389c454c73e
tomoyat <1283480+tomoyat@users.noreply.github.com>
2024-01-28 16:37:18 +0900

    refactoring

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import type {Post} from "$lib/posts";
+    import Article from '$lib/components/Article.svelte';
 
     export let data;
     const post: Post = data.post;
@@ -16,11 +17,4 @@
     {/if}
 </svelte:head>
 
-<article class="mt-12">
-    <h1 class="font-medium text-2xl text-navy-dark text-center">{ post.title }</h1>
-    <p class="text-sm text-navy-light mt-2 text-right">{post.publishedAtString}</p>
-    <div class="prose mt-12 tracking-wider text-justify max-w-none prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg
-      prose-p:text-base prose-p:font-light prose-a:text-base prose-li:text-base prose-li:font-light">
-        <svelte:component this={post.component}/>
-    </div>
-</article>
+<Article post={post} />
