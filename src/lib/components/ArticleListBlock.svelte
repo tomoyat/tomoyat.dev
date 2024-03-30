3452e02c90a018cc0aeb2c47e1805685ba03e4f7
tomoyat <1283480+tomoyat@users.noreply.github.com>
2024-03-31 01:59:37 +0900

    Reformat

--- src/lib/components/ArticleListBlock.svelte
+++ src/lib/components/ArticleListBlock.svelte
@@ -7,14 +7,14 @@
 
 <div class="mt-8 border-l-2 border-blue px-4 py-2">
   <a href="{linkUrl}">
-  <div class="flex flex-col sm:flex-row sm:justify-between sm:items-baseline">
-    <h2 class="text-xl text-navy-dark">{post.title}</h2>
-    <div class="mt-1 text-sm text-navy-light">{post.publishedAtString}</div>
-  </div>
-  {#if post.description}
-    <p class="mt-4 text-sm text-navy-light">
-      {post.description}
-    </p>
-  {/if}
+    <div class="flex flex-col sm:flex-row sm:justify-between sm:items-baseline">
+      <h2 class="text-xl text-navy-dark">{post.title}</h2>
+      <div class="mt-1 text-sm text-navy-light">{post.publishedAtString}</div>
+    </div>
+    {#if post.description}
+      <p class="mt-4 text-sm text-navy-light">
+        {post.description}
+      </p>
+    {/if}
   </a>
 </div>
