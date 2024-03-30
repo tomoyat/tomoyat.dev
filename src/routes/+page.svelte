46f7f98dac718c064435cd635140107cc8643d35
tomoyat <1283480+tomoyat@users.noreply.github.com>
2024-03-31 01:33:44 +0900

    Refactor post listing markup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,17 @@
 <script lang="ts">
-    import type {Post} from '$lib/posts'
+  import type { Post } from "$lib/posts";
 
-    export let data;
-    let posts: Post[] = data.posts;
-    posts = posts.filter((p) => p.type == "tech")
-    posts.sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime())
+  export let data;
+  let posts: Post[] = data.posts;
+  posts = posts.filter((p) => p.type == "tech");
+  posts.sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
 </script>
 
-<div class="mt-4">
-    {#each posts as post, i}
-        {#if i < 1}
-            <div class="mt-12">
-                <a href="/posts/{post.slug}"><h2 class="text-xl text-navy-dark">{post.title}</h2></a>
-                <div class="mt-1 text-sm text-navy-light">{post.publishedAtString}</div>
-            </div>
-        {:else}
-            <div class="mt-8">
-                <a href="/posts/{post.slug}"><h2 class="text-xl text-navy-dark">{post.title}</h2></a>
-                <div class="mt-1 text-sm text-navy-light">{post.publishedAtString}</div>
-            </div>
-        {/if}
-    {/each}
+<div class="pt-4">
+  {#each posts as post}
+    <div class="mt-8">
+      <a href="/posts/{post.slug}"><h2 class="text-xl text-navy-dark">{post.title}</h2></a>
+      <div class="mt-1 text-sm text-navy-light">{post.publishedAtString}</div>
+    </div>
+  {/each}
 </div>
