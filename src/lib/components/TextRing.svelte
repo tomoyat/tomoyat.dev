44395230cf64d46977afc8e2c1ea93d0356c53e8
tomoyat <1283480+tomoyat@users.noreply.github.com>
2024-06-03 00:36:17 +0900

    Add TextRing svelte component
    A new svelte component named `TextRing` has been added. This component allows for text to be displayed in a circular shape with customizable size.

---
+++ src/lib/components/TextRing.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+
+	export let text: string = 'Happy Hacking !  ';
+	export let charSize: number = 1.5;
+
+	function radius(): number {
+		return charSize / (2.0 * Math.sin(Math.PI / text.length));
+	}
+</script>
+
+<div>
+	{#if text.length > 0}
+	<span class="text-ring spin">
+		{#each text as char, index}
+			<span class="text-ring-ch" style="--index: {index}; --total: {text.length}; --radius: {radius()}">{char}</span>
+		{/each}
+	</span>
+	{/if}
+</div>
+
+<style>
+    .text-ring-ch {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%) rotate(calc(360deg / var(--total) * var(--index))) translateY(calc(var(--radius) * -1ch))
+    }
+</style>
